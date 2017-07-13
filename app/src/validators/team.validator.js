138c4352809c1de1d467758c6b3285f305bd362e
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-07-13 15:02:29 +0200

    Validate isArray on patch too

--- app/src/validators/team.validator.js
+++ app/src/validators/team.validator.js
@@ -3,12 +3,6 @@ const ErrorSerializer = require('serializers/error.serializer');
 
 class TeamValidator {
     static async create(ctx, next) {
-        logger.info('Validating body for create team');
-        ctx.checkBody('name').notEmpty().len(1, 100);
-        ctx.checkBody('managers').optional();
-        ctx.checkBody('users').optional();
-        ctx.checkBody('areas').optional();
-        ctx.checkBody('layers').optional();
         const isArrayToErrors = (field) => {
           const value = ctx.request.body[field];
           if (typeof value !== 'undefined' && !Array.isArray(value)) {
@@ -16,6 +10,12 @@ class TeamValidator {
             ctx.errors = ctx.errors ? [ ...ctx.errors , newError ]: [ newError ];
           }
         }
+        logger.info('Validating body for create team');
+        ctx.checkBody('name').notEmpty().len(1, 100);
+        ctx.checkBody('managers').optional();
+        ctx.checkBody('users').optional();
+        ctx.checkBody('areas').optional();
+        ctx.checkBody('layers').optional();
         isArrayToErrors('managers');
         isArrayToErrors('users');
         isArrayToErrors('areas');
@@ -30,6 +30,13 @@ class TeamValidator {
     }
 
     static async update(ctx, next) {
+        const isArrayToErrors = (field) => {
+          const value = ctx.request.body[field];
+          if (typeof value !== 'undefined' && !Array.isArray(value)) {
+            const newError = { [field]: `${field} should be an Array.` };
+            ctx.errors = ctx.errors ? [ ...ctx.errors , newError ]: [ newError ];
+          }
+        }
         logger.info('Validating body for update team');
         ctx.checkBody('name').optional().notEmpty().len(1, 100);
         ctx.checkBody('managers').optional();
@@ -37,6 +44,11 @@ class TeamValidator {
         ctx.checkBody('users').optional();
         ctx.checkBody('areas').optional();
         ctx.checkBody('layers').optional();
+
+        isArrayToErrors('managers');
+        isArrayToErrors('users');
+        isArrayToErrors('areas');
+        isArrayToErrors('layers');
 
         if (ctx.errors) {
             ctx.body = ErrorSerializer.serializeValidationBodyErrors(ctx.errors);
