1b9bbe1172ce54310570f62d79dfe8a751540141
Alvaro <pursuance@gmail.com>
2017-07-27 12:56:50 +0200

    Revert "Feature/delete users from previous teams"

--- app/src/services/team.service.js
+++ app/src/services/team.service.js
@@ -3,7 +3,6 @@ const JWT = require('jsonwebtoken');
 const config = require('config');
 const MailService = require('services/MailService');
 const UserService = require('services/user.service');
-const TeamModel = require('models/team.model');
 
 class TeamService {
   
@@ -39,21 +38,6 @@ class TeamService {
       const managerEmail = UserService.getEmailById(managerId);
       MailService.sendMail(joinedMailId, { email: confirmedUserEmail }, [{ address: { managerEmail } }]);
     });
-  }
-
-  static async deleteConfirmedUserFromPreviousTeams(userId, teamId) {
-    const userTeams = await TeamModel.find({ confirmedUsers: userId });
-    logger.info(`User was in ${userTeams}`);
-    userTeams.forEach( async (team) => {
-      if (team.id !== teamId) {
-        const teamToChange = await TeamModel.findById(team.id);
-        logger.info(`Team id ${teamToChange.id}`);
-        teamToChange.confirmedUsers = teamToChange.confirmedUsers.filter((user) => user !== userId);
-        logger.info(`User ${userId} is not in ${teamToChange.confirmedUsers}`);
-        await teamToChange.save;
-      }
-    });
-    logger.info(`User is in ${userTeams}`);
   }
 }
 module.exports = TeamService;
