a5775fd90f8215331815c85e2e414dce2e6ba7bd
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-07-27 13:54:42 +0200

    Confirm if its only the same email

--- app/src/routes/api/v1/team.router.js
+++ app/src/routes/api/v1/team.router.js
@@ -34,8 +34,9 @@ class TeamRouter {
       if (!userId) ctx.throw(400, 'User missing');
       if (data) {
         const { email, teamId } = data;
+        const userEmail = await UserService.getEmailById(userId);
+        if (userEmail !== email) ctx.throw(400, 'Email to confirm and your email do not match');
         const team = await TeamModel.findById(teamId);
-
         if (team && !team.confirmedUsers.includes(email)) {
           TeamService.deleteConfirmedUserFromPreviousTeams(userId, teamId);
           team.users = team.users.filter(user => user !== email);
